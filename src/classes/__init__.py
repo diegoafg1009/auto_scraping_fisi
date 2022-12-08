0323fae9ddd621e9ab866839ff3f8a0ab578ed0f
diegoam11 <diegoalvarezmore@gmail.com>
2022-12-08 13:48:25 -0500

    se agregó search_kavak_ 2 para una búsqueda más rápida

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -2,5 +2,6 @@ from .auto import Auto
 from .search import Search
 from .search_neo_auto import SearchNeoAuto
 from .search_kavak import SearchKavak
+from .search_kavak_2 import SearchKavak2
 from .user import User
 from .config_file import ConfigFile
