a9b00bb734a132d9a9dcaff7998c8f4b22a4d705
diegoam11 <diegoalvarezmore@gmail.com>
2022-12-15 02:56:34 -0500

    Se valido la opcion de agregar a favoritos

--- src/classes/search_kavak.py
+++ src/classes/search_kavak.py
@@ -5,36 +5,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-import time
+from re import split
+
 
 class SearchKavak(Search):
 
     def __init__(self, brand: str, model: str, from_year: int, until_year: int, quantity: int):
         Search.__init__(self, brand, model, from_year, until_year, quantity)
-        self.__site = "https://www.kavak.com/pe/carros-usados"
+        self.__site = f"https://www.kavak.com/pe/autos-{self._model}/orden-menor-precio/carros-usados"
         self.__driver = None
         self.autos = []
 
     def _filter(self):
         options = Options()
-        #options.add_argument('--headless')
-        options.add_argument("--window-size=1920,1080")
-        self.__driver = webdriver.Chrome(service=Service(self.__path), options=options)
+        options.add_argument('--headless')
+        self.__driver = webdriver.Chrome(service=Service(self._path), options=options)
+        self.__site = self.__get_site_fixed(self._model)
         self.__driver.get(self.__site)
-        time.sleep(10)
-
-        wait = WebDriverWait(self.__driver, 10)
-        wait.until(ec.element_to_be_clickable((By.XPATH, "//h3[@data-cy='btn-accordion-año']"))).click()
-        self.__select_year()
-        wait.until(ec.element_to_be_clickable((By.XPATH, "//h3[@data-cy='btn-accordion-año']"))).click()
-        wait.until(ec.element_to_be_clickable((By.XPATH, "//h3[@data-cy='btn-accordion-marca-y-modelo']"))).click()
-        wait.until(ec.element_to_be_clickable((By.XPATH, f"//img[@alt='{self._brand.capitalize()}']"))).click()
-        wait.until(ec.element_to_be_clickable((By.XPATH, f"//button[@aria-label='Select {self._model.capitalize()}']"))).click()
-        wait.until(ec.element_to_be_clickable((By.XPATH, "//span[@class='select-value catalogue']"))).click()
-        wait.until(ec.element_to_be_clickable((By.XPATH, "//button[@aria-label='lowerPrice']"))).click()
-        time.sleep(3)
+        try:
+            if self.__verify_brand() is False:
+                print('No se encontro la marca buscada')
+                return False
+        except:
+            print('No se encontro el modelo buscado')
+            return False
 
     def __get_url(self):
         elements_with_urls = self.__driver.find_elements(
@@ -43,40 +37,64 @@ class SearchKavak(Search):
         return urls
 
     def __get_price(self):
+        new_prices = []
         elements_with_prices = self.__driver.find_elements(
             By.XPATH, value="//div[1][@class='payment-tax-wrapper']//span[@class='payment-total payment-highlight']")
         prices = [price.text for price in elements_with_prices]
-        return prices
+        for text in prices:
+            no_dollar = text.replace('US$ ', '')
+            no_dollar_coma = no_dollar.replace(',', '.')
+            if no_dollar_coma != '':
+                new_prices.append(no_dollar_coma)
+        return new_prices
 
     def __get_year(self):
+        only_years = []
         elements_with_years = self.__driver.find_elements(
             By.XPATH, value="//p[@class='car-details']")
         years = [year.text for year in elements_with_years]
-        return years
+        for text in years:
+            result = split('\D+ ', text)
+            only_years.append(result[0])
+        return only_years
 
     def get_autos(self):
         i = 0
-        self._filter()
-        link_list = self.__get_url()
-        price_list = self.__get_price()
-        year_list = self.__get_year()
-
-        while i < self._quantity:
-            if i < len(link_list):
+        if self._filter() is False:
+            return None
+        else:
+            link_list = self.__get_url()
+            price_list = self.__get_price()
+            year_list = self.__get_year()
+            while i < len(price_list):
                 link = link_list[i]
                 price = price_list[i]
                 year = year_list[i]
-                auto = Auto(self._brand, self._model, year, price, link)
-                self.autos.append(auto)
+                auto = Auto(self._brand.capitalize(), self._model.capitalize(), year, price, link)
+                if (int(auto.get_year()) >= self._from_year) and (int(auto.get_year()) <= self._until_year):
+                    self.autos.append(auto)
                 i += 1
-            else:
-                break
-        return self.autos
+            self.autos.sort(key=lambda p: p.get_price())
+            correct_list = self.__split_list(self.autos, self._quantity)
+            return correct_list
 
-    def __select_year(self):
-        conteinner = self.__driver.find_element(
-            By.XPATH, value="//app-facet-attr")
-        buttons = conteinner.find_elements(By.CSS_SELECTOR, value="button[class='no-focus btn-accent sc-kavak-button']")
-        for button in buttons:
-            if (int(button.text) >= self._from_year) and (int(button.text) <= self._until_year):
-                button.click()
+    def __get_site_fixed(self, model: str):
+        if ' ' in model:
+            words = split(' ', model)
+            self.__site = f"https://www.kavak.com/pe/autos-{words[0]}_{words[1]}/orden-menor-precio/carros-usados"
+        return self.__site
+
+    def __split_list(self, objects: list, until):
+        return objects[:until]
+
+    def __verify_brand(self):
+        car_name = self.__driver.find_element(By.XPATH, value="//h2[@class='car-name']").text
+        car_name = car_name.split(maxsplit=2)
+        first_part = car_name[0].lower()
+        second_part = car_name[1].lower()
+        complete = first_part + ' ' + second_part
+        self._brand = self._brand.lower()
+        if (self._brand == first_part) or (self._brand == complete):
+            return True
+        else:
+            return False
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from classes import *
 def menu():
     while True:
         print("Bienvenido")
-        print("==========")
+        print("=================")
         print("1. Iniciar Sesion")
         print("2. Registrarse")
         print("3. Salir")
@@ -31,7 +31,7 @@ def menu():
 def sub_menu(user_id):
     while True:
         print("Auto-Scraping FISI")
-        print("==================")
+        print("=========================================")
         print("1. Realizar Busqueda Neoauto")  # change
         print("2. Realizar Busqueda Kavak")  # change
         print("3. Realizar Busqueda Autopia")
@@ -48,21 +48,21 @@ def sub_menu(user_id):
             from_year = int(input("Anio inicial: "))
             until_year = int(input("Anio final: "))
             quantity = int(input("Cantidad de resultados por buscador: "))
-
             search = SearchNeoAuto(brand, model, from_year, until_year, quantity)
-
             autos = search.get_autos()
-
             if autos is not None:
+                i = 1
                 for auto in autos:
+                    print(f"ID: [{i}]")
                     print(f"Marca: {auto.get_brand()}\n Modelo: {auto.get_model()}\n Anio: {auto.get_year()} \n"
                           f" precio: {auto.get_price()} \n url: {auto.get_url()}")
+                    i += 1
                 print(f"Se encontraron {len(autos)} resultados.")
-
-            answer = input("Desea agregar un auto a sus favoritos?(s/n)")
-            if answer == 's' or answer == 'S':
-                id = int(input('Ingrese el id del auto: '))
-                autos[id - 1].add_favorite()
+                print("----------------")
+                answer = input("¿Desea agregar un auto a sus favoritos?(s/n): ")
+                if answer == 's' or answer == 'S':
+                    id = int(input('Ingrese el id del auto: '))
+                    autos[id - 1].add_favorite()
 
         elif option == 2:
             print("Ingrese los datos de su auto: ")
@@ -71,30 +71,41 @@ def sub_menu(user_id):
             from_year = int(input("Anio inicial: "))
             until_year = int(input("Anio final: "))
             quantity = int(input("Cantidad de resultados por buscador: "))
-            search = SearchKavak2(brand, model, from_year, until_year, quantity)
+            search = SearchKavak(brand, model, from_year, until_year, quantity)
             autos = search.get_autos()
             if autos is not None:
+                i = 1
                 for car in autos:
+                    print(f"ID: [{i}]")
                     print(f"Marca: {car.get_brand()}\n Modelo: {car.get_model()}\n Anio: {car.get_year()} \n"
                           f" Precio: {car.get_price()} \n url: {car.get_url()}")
+                    print("----------------")
+                    i += 1
                 print(f"Se encontraron {len(autos)} resultados.")
-            answer = input("Desea agregar un auto a sus favoritos?(s/n)")
-            if answer == 's' or answer == 'S':
-                id = int(input('Ingrese el id del auto: '))
-                autos[id-1].add_favorite()
+                answer = input("¿Desea agregar un auto a sus favoritos?(s/n): ")
+                if answer == 's' or answer == 'S':
+                    id = int(input('Ingrese id del auto: '))
+                    autos[id - 1].add_favorite()
+
         elif option == 3:
+            autos = []
             print("Ingrese los datos de su auto: ")
             brand = input("Marca: ")
-            brand = brand.replace(' ', '%20')  # para formato autopia
             model = input("Modelo: ")
             from_year = int(input("Anio inicial: "))
             until_year = int(input("Anio final: "))
             quantity = int(input("Cantidad de resultados por buscador: "))
-            search = SearchAutopia(brand, model, from_year, until_year, quantity)
-            autos = search.get_autos()
+
+            searchers = [SearchNeoAuto(brand, model, from_year, until_year, quantity),
+                         SearchKavak(brand, model, from_year, until_year, quantity)]
+            for search in searchers:
+                autos.extend(search.get_autos())
             for car in autos:
                 print(f"Marca: {car.get_brand()}\n Modelo: {car.get_model()}\n Anio: {car.get_year()} \n"
                       f" Precio: {car.get_price()} \n url: {car.get_url()}")
+                print("----------------")
+            print(f"Se encontraron {len(autos)} resultados.")
+
         elif option == 4:
             autos = []
             print("Ingrese los datos de su auto: ")
@@ -116,15 +127,18 @@ def sub_menu(user_id):
             if answer == 's' or answer == 'S':
                 id = int(input('Ingrese el id del auto: '))
                 autos[id - 1].add_favorite()
+
         elif option == 5:
-            print("Favoritos:")
+            print("MIS FAVORITOS:")
             favorites = ConfigFile.get_favorites()
             for fav in favorites:
-                print(fav['brand'])
-                print(fav['model'])
-                print(fav['year'])
-                print(fav['price'])
-                print(fav['url'])
+                print("Marca:", fav['brand'])
+                print("Modelo:", fav['model'])
+                print("Anio:", fav['year'])
+                print("Precio:", fav['price'])
+                print("Link:", fav['url'])
+                print("-------------------------")
+
         elif option == 6:
             user = ConfigFile.get_user(user_id)
             user = User(user["_User__id"], user["_User__first_name"], user["_User__last_name"], user["_User__user_name"], user["_User__email"], user["_User__password"])
